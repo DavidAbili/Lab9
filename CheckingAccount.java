6abb96ecce5fe5111a415389aae9290553720e20
DavidAbili <89999138+DavidAbili@users.noreply.github.com>
2021-11-11 16:25:55 -0500

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,4 +1,12 @@
-
+/*Class: CMSC203
+ * Instructor: Dr.Alexander
+ * Due 11/11/21
+ * Platform compiler:
+ * I pledge that I have completed the programming assignment independently
+ * I have not copied the code from a student or any source
+ * I have not given my code to any student
+ * David Abili
+ */
 public class CheckingAccount extends BankAccount {
 	private static final int FEE = 15;
 
